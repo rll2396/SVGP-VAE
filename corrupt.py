19826aedf28be4680da9e9d4a42318ba7aaa52cd
jasonbrooksw <jasonbrooksw@gmail.com>
2020-12-14 22:19:59 -0600

    added framerange option to corrupt_video()

--- corrupt.py
+++ corrupt.py
@@ -10,7 +10,7 @@ import glob
 from pathlib import Path
 from shutil import copyfile
 
-def corrupt_video(filename, frames = "random", frame_percent = 30, video_percent = 30):
+def corrupt_video(filename, frames = "random", frame_percent = 30, video_percent = 30, framerange = 'all'):
     '''
     "Corrupts" video file and returns the video with corrupt frames, as well as the frame rate
     parameters:
@@ -18,6 +18,7 @@ def corrupt_video(filename, frames = "random", frame_percent = 30, video_percent
     frames: a list of specific frames or "random" if a random set of frames are to be corrupted
     frame_percent: percent of each frame to be corrupted
     video_percent: percent of total video to have corrupted frames; ignored if frames is not "random"
+    framerange: 'all' or a list [a, b] that tells the function to take all frames between a and b only
     '''
     vid = imageio.get_reader(filename,  'ffmpeg')
     fps = vid.get_meta_data()['fps']
@@ -26,9 +27,16 @@ def corrupt_video(filename, frames = "random", frame_percent = 30, video_percent
     meta = vid.get_meta_data()
     frate = meta['fps']
     nframes = meta['nframes']
-    frameslist = np.arange(nframes)
+    
+    if framerange == 'all':
+        framerange = range(nframes-2)
+    else:
+        framerange = range(framerange[0],framerange[1])
+        
+    frameslist = np.arange(len(framerange))
     size = meta['size']
-    imlist = np.zeros((nframes-2, size[1], size[0]), dtype=np.float64)
+    imlist = np.zeros((len(framerange), size[1], size[0]), dtype=np.float64)
+    
     if frames == "random":
         random.shuffle(frameslist)
         lim = int(np.round(nframes*video_percent/100))    
@@ -36,7 +44,7 @@ def corrupt_video(filename, frames = "random", frame_percent = 30, video_percent
     else:
         frameslist = frames
 
-    for frame in range(nframes-2):
+    for frame in range(len(framerange)-1):
         a+=1
         random.seed(a=a)
         if frame in frameslist:
@@ -78,27 +86,31 @@ def run():
     # install ffmpeg plugin if needed
     imageio.plugins.ffmpeg.download()
 #     link of the video to be downloaded  
-    link = "https://www.youtube.com/watch?v=QC8iQqtG0hg"
+    link = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+    root = os.getcwd()
+    filename = root + '\Rick Astley - Never Gonna Give You Up (Video).mp4'
+#     link = "https://www.youtube.com/watch?v=QC8iQqtG0hg"
 #     https://stackoverflow.com/questions/49643206/attributeerror-youtube-object-has-no-attribute-get-videos
 #     download video
-    filename = "vid"
-    yt = pytube.YouTube(link)
-    stream = yt.streams.first()
-    stream.download(filename=filename)
-
+    if not os.path.exists(filename):
+        yt = pytube.YouTube(link)
+        stream = yt.streams.first()
+        stream.download()
     
     frames = "random"
-    video_percent = 1
+    video_percent = 0
     frame_percent = 30
     # filename = fpath
+#     filename = root + "\\5 Second Video Watch the Milky Way Rise.mp4"
 #     generate list of all frames from video, takes a couple minutes for the 5 min. video I was testing, and takes 
 # longer if you corrupt more frames. Could probably be improved.
     print("generating corruption")
-    imlist, fps = corrupt_video(filename + ".mp4", frames = frames, frame_percent = frame_percent, video_percent = video_percent)
+    imlist, fps = corrupt_video(filename, frames = frames, frame_percent = frame_percent,
+                                video_percent = video_percent, framerange=[65,185])
     
 #--------------cropping video-------------------------------------------------    
-    horiz_width = 28
-    vert_width = 28
+    horiz_width = 60
+    vert_width = 60
     
     orig_horiz, orig_vert = imlist.shape[1], imlist.shape[2]
     
@@ -108,10 +120,10 @@ def run():
     imlist = imlist[:,hw1:hw2, vw1:vw2]
 #-------------------------------------------------------------------------------
     
+    #videoname = root + "\\test.mp4"
     images = imlist.reshape(imlist.shape+(1,))
+    print(images.shape)
     aux_data = np.arange(len(imlist), dtype=np.float64).reshape(len(imlist), 1)
-    padding = np.empty((len(imlist), 9))
-    aux_data = np.concatenate((aux_data, padding), axis=1)
     print("saving video")
 #     save video, takes a while on longer videos
     dataset = {'images':images, 'aux_data':aux_data}
@@ -122,7 +134,6 @@ def run():
     copyfile("test data/train_data3.p", "test data/eval_data3.p")
     copyfile("MNIST data/train_ids_mask3.p", "test data/train_ids_mask3.p")
     
+    
 if __name__ == "__main__":
     run()
-    
-    
